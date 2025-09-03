eb2a2096179b172fc7a1c95ea32ebeaea095988d
Evgenii Kondratiev <9219210217@yandex.ru>
2025-09-03 13:01:29 +0300

    добавленаопция --format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,8 +4,16 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("first_file", type=int, help="first_argument")
-    parser.add_argument("second_file", type=int, help="second_argument")
+    parser.add_argument("first_file", type=int)
+    parser.add_argument("second_file", type=int)
+
+    parser.add_argument(
+            '-f', '--format',
+            metavar='FORMAT',
+            type=str,
+            choices=['plain', 'json'],
+            help='set format of output'
+    )
 
     args = parser.parse_args()
     print(args)
