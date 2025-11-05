c05d9abd098dfc6ef03a9d8c0ffa7f4494ce165f
Evgenii Kondratiev <9219210217@yandex.ru>
2025-11-05 19:46:47 +0300

    added expected files and added parametrises

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,22 +3,38 @@ import pytest
 from gendiff.scripts.gendiff import generate_diff
 
 
-@pytest.mark.parametrize("file1, file2, expected_result", [
+@pytest.mark.parametrize("file1, file2, formatter, expected_result", [
     (
         "tests/fixtures/file1.json", 
-     "tests/fixtures/file2.json", 
+     "tests/fixtures/file2.json", "json",
      "tests/expected_result_json.txt"
      ),
      (
-        "tests/fixtures/file1.yaml", 
-     "tests/fixtures/file2.yaml", 
-     "tests/expected_result_yaml.txt"
+        "tests/fixtures/file1.json", 
+     "tests/fixtures/file2.json", "plain",
+     "tests/expected_result_plain.txt"
+     ),
+     (
+        "tests/fixtures/file1.json", 
+     "tests/fixtures/file2.json", "stylish",
+     "tests/expected_result_stylish.txt"
      ),
      (
         "tests/fixtures/file1_nested.json", 
-     "tests/fixtures/file2_nested.json", 
+     "tests/fixtures/file2_nested.json", "json"
      "tests/expected_result_nested_json.txt"
+     ),
+     (
+        "tests/fixtures/file1_nested.json", 
+     "tests/fixtures/file2_nested.json", "plain"
+     "tests/expected_result_nested_plain.txt"
+     ),
+     (
+        "tests/fixtures/file1_nested.json", 
+     "tests/fixtures/file2_nested.json", "stylish"
+     "tests/expected_result_nested_stylilsh.txt"
      )
+
 ])
 def test_gendiff(file1, file2, expected_result):
     diff = generate_diff(file1, file2)
